f4c3d0937d9640ca3fc2a1fc21d15f45effea537
RandomY-2 <69881587+RandomY-2@users.noreply.github.com>
2021-11-18 15:48:07 -0800

    movie recommendations frontend

--- backend/recommender/recommender/settings.py
+++ backend/recommender/recommender/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8flp2wdnjg#1ov@xwbaert()fi*odongcbntsj92z&-aibj^f4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['locahost', '127.0.0.1']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- frontend/recommender/src/App.tsx
+++ frontend/recommender/src/App.tsx
@@ -1,9 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css';
+import {
+    Row,
+    Col,
+    Input,
+    Button,
+    Typography,
+} from 'antd';
+import axios from 'axios';
 
 const App = () => {
-    return (
-        <div>
+    const baseURL: String = 'http://127.0.0.1:8000/api';
+    const [movieName, setMovieName] = useState<String>("");
+    const [movieRecommendations, setMovieRecommendations] = useState<String[]>([]);
+    const [selectedMovie, setSelectedMovie] = useState<String>("");
 
+    const handleMovieSearch = async (): Promise<void> => {
+        try {
+            const res = await axios.post(baseURL + '/movie_suggestions', {
+                'movie': movieName
+            });
+            setMovieRecommendations(res.data.recommended_movies);
+        } catch (error) {
+            alert(error);
+        }
+    }
+
+    const handleMovieClick = (index: number): void => {
+        setSelectedMovie(movieRecommendations[index]);
+    }
+
+    return (
+        <div className='app-background'>
+            <Row style={{ paddingTop: 40 }}>
+                <Col span={12} offset={6}>
+                    <Input
+                        placeholder='Enter a movie you like to see recommendations'
+                        onChange={(e) => setMovieName(e.target.value)}
+                    />
+                </Col>
+            </Row>
+
+            <Row style={{ marginTop: 40, marginBottom: 70 }}>
+                <Col span={5} offset={10}>
+                    <Button type='primary' onClick={handleMovieSearch}>
+                        Get Recommendations
+                    </Button>
+                </Col>
+            </Row>
+
+            {
+                movieRecommendations.map((movie: String, i: number) => {
+                    return (
+                        <Row style={{ marginTop: 10 }} key={movie as React.Key}>
+                            <Col span={10} offset={10}>
+                                <Typography.Title
+                                    level={5}
+                                    style={{ color: 'white' }}
+                                    onClick={() => handleMovieClick(i)}
+                                >
+                                    {movie}
+                                </Typography.Title>
+                            </Col>
+                        </Row>
+                    )
+                })
+            }
         </div>
     )
 }
